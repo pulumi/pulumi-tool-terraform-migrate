8704539bf7e7a996a7f2b33ace338995e323a47c
VenelinMartinov <venelin.v.martinov@gmail.com>
2025-12-11 21:20:14 +0200

    End-to-end state adapter (#23)
    * End-to-end state adapter
    
    * review

--- pkg/pulumi_providers.go
+++ pkg/pulumi_providers.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pulumi/pulumi-terraform-migrate/pkg/bridgedproviders"
 	"github.com/pulumi/pulumi-terraform-migrate/pkg/providermap"
 	"github.com/pulumi/pulumi-terraform-migrate/pkg/tofu"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
@@ -69,9 +70,28 @@ func pulumiProvidersForTerraformProviders(terraformProviders []providermap.Terra
 
 
 func GetPulumiProvidersForTerraformState(tfState *tfjson.State) (map[providermap.TerraformProviderName]*info.Provider, error) {
+	// TODO[pulumi/pulumi-service#35512: This assumes one Pulumi provider per Terraform provider. This means that provider aliases are not supported.
 	terraformProviders, err := getTerraformProvidersForTerraformState(tfState)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get terraform providers: %w", err)
 	}
 	return pulumiProvidersForTerraformProviders(terraformProviders)
+}
+
+func GetProviderInputs(providerName string) (resource.PropertyMap, error) {
+	// TODO: call the CheckConfig GRPC method
+	switch providerName {
+	case "aws":
+		return resource.PropertyMap{
+			"region":                    resource.NewProperty("us-east-1"),
+			"skipCredentialsValidation": resource.NewProperty(false),
+			"skipRegionValidation":      resource.NewProperty(true),
+			"version":                   resource.NewProperty("7.12.0"),
+		}, nil
+	case "archive":
+		return resource.PropertyMap{
+			"version": resource.NewProperty("0.3.5"),
+		}, nil
+	}
+	return nil, fmt.Errorf("unsupported provider: %s", providerName)
 }
---
+++ pkg/state_adapter.go
@@ -0,0 +1,128 @@
+package pkg
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/valueshim"
+	"github.com/pulumi/pulumi-terraform-migrate/pkg/bridge"
+	"github.com/pulumi/pulumi-terraform-migrate/pkg/providermap"
+	"github.com/pulumi/pulumi-terraform-migrate/pkg/tofu"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+)
+
+type StackExport struct {
+	Deployment apitype.DeploymentV3 `json:"deployment"`
+	Version    int                  `json:"version"`
+}
+
+func TranslateState(tofuProgramDir string, pulumiProgramDir string) (*StackExport, error) {
+	tfState, err := tofu.LoadTerraformState(tofuProgramDir)
+	if err != nil {
+		return nil, err
+	}
+
+	pulumiProviders, err := GetPulumiProvidersForTerraformState(tfState)
+	if err != nil {
+		return nil, err
+	}
+
+	pulumiState, err := convertState(tfState, pulumiProviders)
+	if err != nil {
+		return nil, err
+	}
+
+	deployment, err := MakeDeployment(pulumiState, pulumiProgramDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return &StackExport{
+		Deployment: deployment,
+		Version:    3,
+	}, nil
+}
+
+func convertState(tfState *tfjson.State, pulumiProviders map[providermap.TerraformProviderName]*info.Provider) (*PulumiState, error) {
+	pulumiState := &PulumiState{}
+
+	for _, provider := range pulumiProviders {
+		inputs, err := GetProviderInputs(provider.Name)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get provider inputs: %w", err)
+		}
+		uuid := uuid.NewString()
+		pulumiState.Providers = append(pulumiState.Providers, PulumiResource{
+			ID:      uuid,
+			Type:    "pulumi:providers:" + provider.Name,
+			Name:    "default_" + strings.ReplaceAll(provider.Version, ".", "_"),
+			Inputs:  inputs,
+			Outputs: inputs,
+		})
+	}
+
+	err := tofu.VisitResources(tfState, func(resource *tfjson.StateResource) error {
+		pulumiResource, err := convertResourceState(resource, pulumiProviders)
+		if err != nil {
+			return fmt.Errorf("failed to convert resource state for %s with ID %s: %w", resource.Type, resource.Address, err)
+		}
+		pulumiState.Resources = append(pulumiState.Resources, pulumiResource)
+		return nil
+	}, &tofu.VisitOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to visit resources: %w", err)
+	}
+
+	return pulumiState, nil
+}
+
+func convertResourceState(res *tfjson.StateResource, pulumiProviders map[providermap.TerraformProviderName]*info.Provider) (PulumiResource, error) {
+	prov, ok := pulumiProviders[providermap.TerraformProviderName(res.ProviderName)]
+	if !ok {
+		return PulumiResource{}, fmt.Errorf("no Pulumi provider found for Terraform provider: %s", res.ProviderName)
+	}
+	shimResource := prov.P.ResourcesMap().Get(res.Type)
+	if shimResource == nil {
+		return PulumiResource{}, fmt.Errorf("no resource type found for Terraform resource: %s", res.Type)
+	}
+
+	ctyResourceType, err := valueshim.ToCtyType(shimResource.SchemaType())
+	if err != nil {
+		return PulumiResource{}, fmt.Errorf("failed to convert resource type to CTY type: %w", err)
+	}
+
+	ctyValue, err := tofu.StateToCtyValue(res, ctyResourceType)
+	if err != nil {
+		return PulumiResource{}, fmt.Errorf("failed to convert resource to CTY value: %w", err)
+	}
+
+	pulumiTypeToken, err := bridge.PulumiTypeToken(res.Type, prov)
+	if err != nil {
+		return PulumiResource{}, fmt.Errorf("failed to get Pulumi type token: %w", err)
+	}
+	resourceInfo := prov.Resources[res.Type]
+	props, err := convertTFValueToPulumiValue(ctyValue, shimResource, resourceInfo)
+	if err != nil {
+		return PulumiResource{}, fmt.Errorf("failed to convert value to Pulumi value: %w", err)
+	}
+
+	inputs, err := tfbridge.ExtractInputsFromOutputs(resource.PropertyMap{}, props, shimResource.Schema(), resourceInfo.Fields, false)
+	if err != nil {
+		return PulumiResource{}, fmt.Errorf("failed to extract inputs from outputs: %w", err)
+	}
+
+	return PulumiResource{
+		ID:      props["id"].StringValue(),
+		Type:    string(pulumiTypeToken),
+		Inputs:  inputs,
+		Outputs: props,
+		Name:    res.Name,
+		// Parent:   stackUrn,
+		// Provider: providerUrn,
+	}, nil
+}
