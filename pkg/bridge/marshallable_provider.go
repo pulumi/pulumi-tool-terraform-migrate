416f684ffd2d7ae4e97aab171b73604dcc976217
VenelinMartinov <venelin.v.martinov@gmail.com>
2025-12-11 13:03:37 +0200

    Unmarshallable Provider (#15)

---
+++ pkg/bridge/marshallable_provider.go
@@ -0,0 +1,98 @@
+package pkg
+
+import (
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/schema"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/valueshim"
+)
+
+// MarshallableProvider is a JSON-marshallable form of a Pulumi provider.
+// This contrasts with the type in tfbridge/info, with the fact that the Resource also contains a SchemaType field.
+// TODO: We might be able to get rid of this and call the bridge [info.MarshallableProvider] instead, using [ImpliedType]
+// directly for producing the [cty.Type] instead of relying on the [SchemaType] method.
+type MarshallableProvider struct {
+	Provider          *MarshallableProviderShimWithTypes      `json:"provider"`
+	Name              string                                  `json:"name"`
+	Version           string                                  `json:"version"`
+	Config            map[string]*info.MarshallableSchema     `json:"config,omitempty"`
+	Resources         map[string]*info.MarshallableResource   `json:"resources,omitempty"`
+	DataSources       map[string]*info.MarshallableDataSource `json:"dataSources,omitempty"`
+	TFProviderVersion string                                  `json:"tfProviderVersion,omitempty"`
+}
+
+func (m *MarshallableProvider) Unmarshal() *info.Provider {
+	config := make(map[string]*info.Schema)
+	for k, v := range m.Config {
+		config[k] = v.Unmarshal()
+	}
+	resources := make(map[string]*info.Resource)
+	for k, v := range m.Resources {
+		resources[k] = v.Unmarshal()
+	}
+	dataSources := make(map[string]*info.DataSource)
+	for k, v := range m.DataSources {
+		dataSources[k] = v.Unmarshal()
+	}
+
+	info := info.Provider{
+		P:                 m.Provider.Unmarshal(),
+		Name:              m.Name,
+		Version:           m.Version,
+		Config:            config,
+		Resources:         resources,
+		DataSources:       dataSources,
+		TFProviderVersion: m.TFProviderVersion,
+	}
+
+	return &info
+}
+
+// MarshallableProviderShim is the JSON-marshallable form of a Terraform provider schema.
+// This contrasts with the type in tfbridge/info, with the fact that the Resource also contains a SchemaType field.
+type MarshallableProviderShimWithTypes struct {
+	Schema      map[string]*info.MarshallableSchemaShim      `json:"schema,omitempty"`
+	Resources   map[string]MarshallableResourceShimWithTypes `json:"resources,omitempty"`
+	DataSources map[string]MarshallableResourceShimWithTypes `json:"dataSources,omitempty"`
+}
+
+// Unmarshal creates a mostly-initialized Terraform provider schema from a MarshallableProvider
+func (m *MarshallableProviderShimWithTypes) Unmarshal() shim.Provider {
+	if m == nil {
+		return nil
+	}
+
+	config := schema.SchemaMap{}
+	for k, v := range m.Schema {
+		config[k] = v.Unmarshal()
+	}
+	resources := schema.ResourceMap{}
+	for k, v := range m.Resources {
+		resources[k] = v.Unmarshal()
+	}
+	dataSources := schema.ResourceMap{}
+	for k, v := range m.DataSources {
+		dataSources[k] = v.Unmarshal()
+	}
+	return (&schema.Provider{
+		Schema:         config,
+		ResourcesMap:   resources,
+		DataSourcesMap: dataSources,
+	}).Shim()
+}
+
+// MarshallableResourceShimWithTypes is a JSON-marshallable form of a Terraform resource schema.
+// This contrasts with the type in tfbridge/info, with the fact that the Resource also contains a SchemaType field.
+type MarshallableResourceShimWithTypes map[string]*info.MarshallableSchemaShim
+
+// Unmarshal creates a mostly-initialized Terraform resource schema from the given MarshallableResourceShim.
+func (m MarshallableResourceShimWithTypes) Unmarshal() shim.Resource {
+	s := schema.SchemaMap{}
+	for k, v := range m {
+		s[k] = v.Unmarshal()
+	}
+	return (&schema.Resource{
+		Schema:     s,
+		SchemaType: valueshim.FromCtyType(ImpliedType(s, true)),
+	}).Shim()
+}
---
+++ pkg/bridge/marshallable_provider_test.go
@@ -0,0 +1,113 @@
+package pkg
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	schemashim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/schema"
+)
+
+func TestMarshallableResourceShimSchemaType(t *testing.T) {
+	t.Parallel()
+
+	t.Run("plain attributes", func(t *testing.T) {
+		t.Parallel()
+		resource := schemashim.Resource{
+			Schema: schemashim.SchemaMap{
+				"name": (&schemashim.Schema{
+					Type: shim.TypeString,
+				}).Shim(),
+			},
+		}
+
+		marshalledResource := info.MarshalResourceShim(resource.Shim())
+		jsonBlob, err := json.Marshal(marshalledResource)
+		if err != nil {
+			t.Fatalf("failed to marshal marshalledResource: %v", err)
+		}
+
+		unmarshallableResource := MarshallableResourceShimWithTypes{}
+		err = json.Unmarshal(jsonBlob, &unmarshallableResource)
+		if err != nil {
+			t.Fatalf("failed to unmarshal marshalledResource: %v", err)
+		}
+		unmarshalledResource := unmarshallableResource.Unmarshal()
+
+		autogold.Expect(`cty.Object(map[string]cty.Type{"id":cty.String, "name":cty.String, "timeouts":cty.Object(map[string]cty.Type{"create":cty.String, "default":cty.String, "delete":cty.String, "read":cty.String, "update":cty.String})})`).Equal(t, unmarshalledResource.SchemaType().GoString())
+	})
+
+	t.Run("collections", func(t *testing.T) {
+		t.Parallel()
+		resource := schemashim.Resource{
+			Schema: schemashim.SchemaMap{
+				"x": (&schemashim.Schema{
+					Type: shim.TypeList,
+					Elem: (&schemashim.Schema{
+						Type: shim.TypeString,
+					}).Shim(),
+				}).Shim(),
+				"y": (&schemashim.Schema{
+					Type: shim.TypeSet,
+					Elem: (&schemashim.Schema{
+						Type: shim.TypeString,
+					}).Shim(),
+				}).Shim(),
+				"z": (&schemashim.Schema{
+					Type: shim.TypeMap,
+					Elem: (&schemashim.Schema{
+						Type: shim.TypeString,
+					}).Shim(),
+				}).Shim(),
+			},
+		}
+
+		marshalledResource := info.MarshalResourceShim(resource.Shim())
+		jsonBlob, err := json.Marshal(marshalledResource)
+		if err != nil {
+			t.Fatalf("failed to marshal marshalledResource: %v", err)
+		}
+		unmarshallableResource := MarshallableResourceShimWithTypes{}
+		err = json.Unmarshal(jsonBlob, &unmarshallableResource)
+		if err != nil {
+			t.Fatalf("failed to unmarshal unmarshalledResource: %v", err)
+		}
+		unmarshalledResource := unmarshallableResource.Unmarshal()
+
+		autogold.Expect(`cty.Object(map[string]cty.Type{"id":cty.String, "timeouts":cty.Object(map[string]cty.Type{"create":cty.String, "default":cty.String, "delete":cty.String, "read":cty.String, "update":cty.String}), "x":cty.List(cty.String), "y":cty.Set(cty.String), "z":cty.Map(cty.String)})`).Equal(t, unmarshalledResource.SchemaType().GoString())
+	})
+
+	t.Run("blocks", func(t *testing.T) {
+		t.Parallel()
+		resource := schemashim.Resource{
+			Schema: schemashim.SchemaMap{
+				"x": (&schemashim.Schema{
+					Type: shim.TypeList,
+					Elem: (&schemashim.Resource{
+						Schema: schemashim.SchemaMap{
+							"y": (&schemashim.Schema{
+								Type: shim.TypeString,
+							}).Shim(),
+						},
+					}).Shim(),
+				}).Shim(),
+			},
+		}
+
+		marshalledResource := info.MarshalResourceShim(resource.Shim())
+		jsonBlob, err := json.Marshal(marshalledResource)
+		if err != nil {
+			t.Fatalf("failed to marshal marshalledResource: %v", err)
+		}
+		unmarshallableResource := MarshallableResourceShimWithTypes{}
+		err = json.Unmarshal(jsonBlob, &unmarshallableResource)
+		if err != nil {
+			t.Fatalf("failed to unmarshal marshalledResource: %v", err)
+		}
+		unmarshalledResource := unmarshallableResource.Unmarshal()
+
+		autogold.Expect(`cty.Object(map[string]cty.Type{"id":cty.String, "timeouts":cty.Object(map[string]cty.Type{"create":cty.String, "default":cty.String, "delete":cty.String, "read":cty.String, "update":cty.String}), "x":cty.List(cty.Object(map[string]cty.Type{"y":cty.String}))})`).Equal(t, unmarshalledResource.SchemaType().GoString())
+	})
+}
