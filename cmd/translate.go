9479f636b218838cd5441a554a5f2384e45c4672
Venelin Martinov <venelin@pulumi.com>
2025-12-11 15:16:50 +0200

    Move state file writing into pkg/

--- cmd/translate.go
+++ cmd/translate.go
@@ -4,9 +4,7 @@ Copyright © 2025 Pulumi Corporation
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/pulumi/pulumi-terraform-migrate/pkg"
 	"github.com/spf13/cobra"
@@ -29,18 +27,9 @@ Example:
 		fmt.Printf("Converting Terraform state from: %s\n", inputPath)
 		fmt.Printf("Output will be written to: %s\n", outputFile)
 
-		data, err := pkg.TranslateState(inputPath, stackFolder)
+		err := pkg.TranslateAndWriteState(inputPath, stackFolder, outputFile)
 		if err != nil {
-			return fmt.Errorf("failed to convert Terraform state: %w", err)
-		}
-
-		bytes, err := json.Marshal(data)
-		if err != nil {
-			return fmt.Errorf("failed to marshal Pulumi state: %w", err)
-		}
-		err = os.WriteFile(outputFile, bytes, 0o600)
-		if err != nil {
-			return fmt.Errorf("failed to write Pulumi state: %w", err)
+			return fmt.Errorf("failed to convert and write Terraform state: %w", err)
 		}
 		return nil
 	},
