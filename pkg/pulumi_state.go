a018fa2fc83cda3b19525edac9333b04febec8ae
Anton Tayanovskyy <anton@pulumi.com>
2025-12-16 14:15:23 -0500

    Revert "fix automation API issue with stack name (#38)" (#40)
    This reverts commit 1d4193cb6c323682b31ca11ec7f54d8cc616cc44.

--- pkg/pulumi_state.go
+++ pkg/pulumi_state.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os/exec"
-	"strings"
 	"time"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
@@ -32,27 +30,6 @@ type PulumiState struct {
 	Resources []PulumiResource
 }
 
-func getStackName(projectFolder string) (string, error) {
-	cmd := exec.Command("pulumi", "stack")
-	cmd.Dir = projectFolder
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to get stack name: %w", err)
-	}
-
-	// parse out the stack name from the first line:
-	// `Current stack is dev:`
-	lines := strings.Split(string(output), "\n")
-	if len(lines) == 0 {
-		return "", fmt.Errorf("no stack name found")
-	}
-	if !strings.HasPrefix(lines[0], "Current stack is ") {
-		return "", fmt.Errorf("no stack name found")
-	}
-	stackName := strings.TrimSpace(lines[0][len("Current stack is ") : len(lines[0])-1])
-	return stackName, nil
-}
-
 func GetDeployment(outputFolder string) (apitype.DeploymentV3, error) {
 	ctx := context.Background()
 	workspace, err := auto.NewLocalWorkspace(ctx, auto.WorkDir(outputFolder))
@@ -60,12 +37,12 @@ func GetDeployment(outputFolder string) (apitype.DeploymentV3, error) {
 		return apitype.DeploymentV3{}, fmt.Errorf("failed to create workspace: %w", err)
 	}
 
-	stackName, err := getStackName(outputFolder)
+	currentStack, err := workspace.Stack(context.TODO())
 	if err != nil {
-		return apitype.DeploymentV3{}, fmt.Errorf("failed to get stack name: %w", err)
+		return apitype.DeploymentV3{}, fmt.Errorf("failed to get current stack: %w", err)
 	}
 
-	untypedDeployment, err := workspace.ExportStack(ctx, stackName)
+	untypedDeployment, err := workspace.ExportStack(ctx, currentStack.Name)
 	if err != nil {
 		return apitype.DeploymentV3{}, fmt.Errorf("failed to export stack: %w", err)
 	}
--- pkg/pulumi_state_test.go
+++ pkg/pulumi_state_test.go
@@ -1,14 +1,11 @@
 package pkg
 
 import (
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/hexops/autogold/v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/stretchr/testify/require"
 )
 
 func TestInsertResourcesIntoDeployment(t *testing.T) {
@@ -85,40 +82,4 @@ func TestInsertResourcesIntoDeployment(t *testing.T) {
 	}
 
 	autogold.ExpectFile(t, data)
-}
-
-func runCommand(t *testing.T, dir string, command string, args ...string) string {
-	t.Helper()
-	cmd := exec.Command(command, args...)
-	cmd.Dir = dir
-	output, err := cmd.Output()
-	if err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			t.Fatalf("failed to run command %s %v, error: %v, output: %s, stdout: %s", command, args, err, string(exitErr.Stderr), output)
-		}
-		t.Fatalf("failed to run command %s %v, error: %v", command, args, err)
-	}
-	return string(output)
-}
-
-func skipIfCI(t *testing.T) {
-	t.Helper()
-	if os.Getenv("CI") == "true" {
-		t.Skip("Skipping test in CI: TODO: set up pulumi credentials in CI")
-	}
-}
-
-func TestGetDeployment(t *testing.T) {
-	skipIfCI(t)
-	testDir, err := os.MkdirTemp("", "test-deployment-*")
-	require.NoError(t, err)
-	defer os.RemoveAll(testDir)
-
-	_ = runCommand(t, testDir, "pulumi", "new", "typescript", "--yes")
-	_ = runCommand(t, testDir, "pulumi", "stack", "select", "dev")
-	_ = runCommand(t, testDir, "pulumi", "up", "--yes")
-
-	deployment, err := GetDeployment(testDir)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(deployment.Resources))
 }
