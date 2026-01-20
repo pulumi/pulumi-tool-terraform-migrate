4d3cab1a8194f2c2ca6e2d713515b3be531b9f26
Anton Tayanovskyy <anton@pulumi.com>
2026-01-20 14:37:51 -0500

    Allow to pass flags to the script

--- scripts/update_providermap.sh
+++ scripts/update_providermap.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 export PULUMI_ADMIN_COMMANDS=true
 
 go build -o ./pulumi-tool-terraform-migrate .
-./pulumi-tool-terraform-migrate update-providermap pkg/providermap/versions.yaml
+./pulumi-tool-terraform-migrate update-providermap pkg/providermap/versions.yaml "$@"
